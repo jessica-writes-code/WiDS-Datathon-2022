4e3077a45303bef9f2c849a5a7a303666e27fd37
Jessica Moore <jmoore523@gmail.com>
2022-01-16 20:21:42 -0500

    Initial commit

---
+++ helpers.py
@@ -0,0 +1,9 @@
+from typing import Tuple
+
+import pandas as pd
+
+
+def load_data() -> Tuple[pd.DataFrame, pd.DataFrame]:
+    train_df = pd.read_csv('./data/train.csv')
+    test_df = pd.read_csv('./data/test.csv')
+    return train_df, test_df
